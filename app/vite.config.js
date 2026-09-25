af209d0598d5a87117b5dad9b84166e4dde3b751
Syed Raqi <syedraqi309@gmail.com>
2026-09-26 02:26:56 +0530

    Update portfolio

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isGitHubActions ? "/Portfolio/" : "/",
   plugins: [react(), tailwindcss()],
 });
